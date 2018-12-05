2b1b9203da287b28886b2abea8e2cf64ccd5eee3
Stach Zagorski <stachzagorski@Stachs-MacBook-Pro.local>
2018-12-04 19:30:03 -0600

    Noted with questions for tomorrow, trying to understand 3d situation

--- www/js/main.js
+++ www/js/main.js
@@ -4,29 +4,36 @@
 const socket = io()
 socket.emit('apiReq', 'https://api.propublica.org/congress/v1/115/senate/members.json')
 socket.on('apiRes', function(json) {
-    const congressMem = json.results[0].congress;
+    //NEED  TO CHANGE const congressMem = json.results[0].congress;
     console.log(json);
-    console.log(congressMem);
+    //NEED TO CHANGE console.log(congressMem);
 
     var camera, scene, renderer, mesh, material, controls;
     init();
     animate();
-    addCubes();
+    angry();
+    fear();
+    joy();
+    sadness();
+    anylitical();
+    confident();
+    tentative();
     render();
 
-
-    function addCubes() {
+//HOW DO I LINK THESE FUNCTIONS UP TO THE MOST REACTED TO EMOTION FROM TONE ANALYZER, THINK I NEED AND "IF THEN"
+//ANGRY FUNCTION WOULD JUST B COPY PASTED FOR EACH EMOTION I THINK
+    function angry() {
     		var xDistance = 10;
         var zDistance = 10;
-        var geometry = new THREE.SphereGeometry(2,50,50);
-        var material = new THREE.MeshBasicMaterial({color:0x0000ff});
+        var geometry = new THREE.SphereGeometry(10,50,50);
+        var material = new THREE.MeshLambertMaterial({color:0xf06d27});
 
         //initial offset so does not start in middle.
         var xOffset = -50;
         var zOffset = -50;
 
-        for(var i = 0; i < Math.sqrt(congressMem); i++){
-            for(var j = 0; j < Math.sqrt(congressMem); j++){
+        //NEED TO CHANGE for(var i = 0; i < Math.sqrt(congressMem); i++){
+            //NEED TO CHANGE for(var j = 0; j < Math.sqrt(congressMem); j++){
             		var mesh  = new THREE.Mesh(geometry, material);
             		mesh.position.x = (xDistance * i) + xOffset;
                 mesh.position.z = (zDistance * j) + zOffset;
@@ -48,19 +55,19 @@ socket.on('apiRes', function(json) {
         camera.position.y = -200;
 
 
-        // Add controls
-        controls = new THREE.TrackballControls( camera );
-        controls.addEventListener( 'change', render );
+        // Add controls I DONT KNOW THAT WE NEED THIS
+        //controls = new THREE.TrackballControls( camera );
+        //controls.addEventListener( 'change', render );
 
         // Create scene.
         scene = new THREE.Scene();
         scene.background = new THREE.Color( 0xffffff );
 
-        // Create ambient light and add to scene.
-        var light = new THREE.AmbientLight(0x404040); // soft white light
-        scene.add(light);
+        // Create ambient light and add to scene. I WANT TO CHANGE THIS WITH THE MOOD READER
+        //var angryLight = new THREE.AmbientLight(0xbb6464); // reddish light
+        //scene.add(angryLight);
 
-        // Create directional light and add to scene.
+        // Create directional light and add to scene. DUNNO IF WE NEED THIS
         var directionalLight = new THREE.DirectionalLight(0xffffff);
         directionalLight.position.set(1, 1, 1).normalize();
         scene.add(directionalLight);
